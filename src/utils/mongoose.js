7869866049fd38f8a097e6c5a27d149caf578f8a
Pelusa-not <agustinlorenzatto@gmail.com>
2024-03-16 10:39:10 -0300

    Probando 2

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -13,7 +13,7 @@ export async function conectDB(){
         return;
     };
 
-    const db = await connect(URL);
+    const db = await connect('mongodb+srv://Agustinlorenzatto:Pelu13524@electro-avenida.wp979qj.mongodb.net/Electro-Avenida?retryWrites=true&w=majority&appName=Electro-Avenid');
     conn.isConnected = db.connections[0].readyState;
     console.log("AAAAAAs")
     console.log(db.connections[0])
