69c594262563f4236260a7747a75422f5cd15846
Pelusa-not <agustinlorenzatto@gmail.com>
2024-03-19 11:23:17 -0300

    Estilice un poco para distintos dipositivos y tambien puse el menuHamn

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export const MenuBar = ({display}) => {
+    
+  return (
+    <div id="menuBar" className={display ? 'flex' : 'hidden'}>
+        <nav className="flex flex-col absolute z-10 bg-yellow-200 right-0.5 px-6 py-2 justify-around gap-5">
+            <Link className="self-center" href='/productos'><p className="text-2xl cursor-pointer hover:text-gray-600 ">Productos</p></Link>
+            <Link className="self-center" href='/productos/ofert'><p className="text-2xl cursor-pointer hover:text-gray-600 self-center">Ofertas</p></Link>
+            <Link className="self-center" href='/contacto'><p className="text-2xl cursor-pointer hover:text-gray-600 self-center">Contacto</p></Link>
+        </nav>
+    </div>
+  )
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,17 @@
+'use client'
 import Link from "next/link"
 import { InputBuscador } from "./InputBuscador"
+import { MenuBar } from "./MenuBar"
+import { useState } from "react"
 
 export const NavBar = () => {
+    const [menuHamb,setMenuHamb] = useState(false);
+
+    const handleMenuHamb = () => {
+        setMenuHamb(!menuHamb);
+    };
+
+
   return (
     <header className="flex bg-yellow-200 pt-2 pb-4 justify-around border-b-2 border-gray-600">
             <Link href={'/'}>
@@ -12,11 +22,18 @@ export const NavBar = () => {
             
             <InputBuscador />
             
-            <nav className="flex justify-around gap-5">
+            <nav id="navegacion" className="flex justify-around gap-5">
                 <Link className="self-center" href='/productos'><p className="text-2xl cursor-pointer hover:text-gray-600 ">Productos</p></Link>
                 <Link className="self-center" href='/productos/ofert'><p className="text-2xl cursor-pointer hover:text-gray-600 self-center">Ofertas</p></Link>
                 <Link className="self-center" href='/contacto'><p className="text-2xl cursor-pointer hover:text-gray-600 self-center">Contacto</p></Link>
             </nav>
+
+            <div id="hamb" className="hidden" onClick={handleMenuHamb}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                <MenuBar display={menuHamb}/>
+            </div>
         </header>
   )
 }
