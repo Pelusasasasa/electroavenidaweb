b76211aa87438f5b9408f4a14db9b7bc7e5785f6
Pelusa-not <agustinlorenzatto@gmail.com>
2024-04-03 19:06:51 -0300

    Puse oara que las busquedas se guarden con la ora de buenos aires

--- src/models/Busqueda.js
+++ src/models/Busqueda.js
@@ -1,10 +1,22 @@
 const { Schema, models, model } = require("mongoose");
 
+function obtenerFechaHoraBuenosAires() {
+    const now = new Date();
+    const nuevaFecha = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString();
+    console.log(nuevaFecha)
+    return nuevaFecha;
+  }
+  
+
 const Busqueda = new Schema({
     texto:{
         type:String,
         required:true
-    },   
+    },
+    createdAt:{
+        type:Date,
+        default:obtenerFechaHoraBuenosAires()
+    }
 },{
     timestamps:true,
 });
